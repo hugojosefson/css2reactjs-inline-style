d9278fd8b345631274f2204344b9d66ac068a287
Hugo Josefson <hugo@josefson.org>
2015-03-12 07:49:37 +0100

    Supports some standard stuff.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+#!/usr/bin/env node
 'use strict';
 
+var linereader = require('through2-linereader');
+var camelCase = require('camel-case');
+var endsWith = require('ends-with');
+
+process.stdin
+    .pipe(linereader())
+    .on('data', function (line) {
+        var l = line.toString();
+        var parts = l.split(': ');
+        var key = camelCase(parts[0]);
+        var value = parts[1];
+        if (endsWith(value, ';')) {
+            value = value.substring(0, value.length - 1);
+        }
+        if (isNaN(Number(value))) {
+            value = '\'' + value + '\'';
+        }
+        console.log(key + ': ' + value);
+    });
